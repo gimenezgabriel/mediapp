b1f57a636a13aeb18b6c41253ae364d7a0203f36
Gabriel <gimenezgabriel705@gmail.com>
2023-08-23 11:07:56 -0300

    primer desafio

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hola Coder!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1EAEAC',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
